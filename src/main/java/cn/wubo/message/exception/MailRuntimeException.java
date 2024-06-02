7de6c9408ca2372a57b8c15f6d8fccd2277611f8
wubo <wubo_aaa@163.com>
2024-06-02 19:31:29 +0800

    去除RuntimeException冗余的方法

--- src/main/java/cn/wubo/message/exception/MailRuntimeException.java
+++ src/main/java/cn/wubo/message/exception/MailRuntimeException.java
@@ -2,10 +2,6 @@ package cn.wubo.message.exception;
 
 public class MailRuntimeException extends RuntimeException {
 
-    public MailRuntimeException(String message) {
-        super(message);
-    }
-
     public MailRuntimeException(String message, Throwable cause) {
         super(message, cause);
     }
